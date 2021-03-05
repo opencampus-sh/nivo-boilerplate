02c163461e39a8c0d43392ca5d3c359b7f425cc0
bagigio <bagigi@disroot.org>
2021-03-05 13:37:56 +0100

    found a simple example and works from sandbox, link added

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # nivo-boilerplate
 just basic responsive bar with nivo as a starter, to be able to graph with mostly python and just a reduced amount of javascript. There is no getting started guide on nivo unfortunately.
 
-Running the code 
-`yarn start`
-should look like this
-![responsivebar](https://user-images.githubusercontent.com/10175720/110040251-c4eb4a80-7d42-11eb-8f70-d4bb328b30aa.png)
+To run the code you can clone the repo and you need to install the dependencies
+You can use 
+```npm install```
+and then
+```npm start```
+and it should start the server locally, available at localhost:3000
+
+The code is also available in a sandbox: --> [sandbox link](https://codesandbox.io/s/nivo-basic-demo-forked-zr2i4) <--
+
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import { ResponsiveBar } from "@nivo/bar";
+import data2 from "./data2.json";
+import data from "./data.json";
+import MyResponsiveBar from "./MyResponsiveBar";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => (
+  <div style={styles}>
+    <h1>Nivo basic demo</h1>
+    <div style={{ height: "400px" }}>
+      <MyResponsiveBar data={data2} />
+    </div>
+    <div style={{ height: "400px" }}>
+      <ResponsiveBar data={data} keys={["earnings"]} indexBy="quarter" />
+    </div>
+  </div>
+);
+
+render(<App />, document.getElementById("root"));
